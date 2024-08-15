6fd2842e0f321c037987301fc02e4400ca42c173
Sefa Saraç <srcsfysf@gmail.com>
2024-08-15 17:31:22 +0300

    Add files via upload

---
+++ List2/List2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> kahveListesi = new List<string>();
+
+        // Kullanıcıdan 5 kahve ismi alalım
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write($"Kahve {i}: ");
+            string kahveIsmi = Console.ReadLine();
+            kahveListesi.Add(kahveIsmi);
+        }
+
+        // foreach döngüsü ile listeyi yazdırma
+        Console.WriteLine("\nGirilen kahve isimleri:");
+        foreach (string kahve in kahveListesi)
+        {
+            Console.WriteLine(kahve);
+        }
+    }
+}
